d7a80b8241e570a487619d14871e4dafda74f9b2
IvanC <ivan.cavlek1403@gmail.com>
2022-11-29 19:36:39 +0100

    Created a workin PDAs app - Test SUCCESS

--- app/ledger/ledger.ts
+++ app/ledger/ledger.ts
@@ -7,4 +7,72 @@ export class Ledger {
     constructor(context: Context) {
         this.context = context;
     }
+
+    async modifyLedger(
+        color: string,
+        newBalance: number,
+        wallet: anchor.web3.Keypair,
+    ) {
+
+        console.log("--------------------------------------------------");
+        let data;
+        let pda = await this.derivePda(color, wallet.publicKey);
+
+        console.log(`Checking if account ${pda} exists for color: ${color}...`);
+        try {
+
+            data = await this.context.program.account.ledger.fetch(pda);
+            console.log("It does.");
+
+        } catch (e) {
+
+            console.log("It does NOT. Creating...");
+            await this.createLedgerAccount(color, pda, wallet);
+            data = await this.context.program.account.ledger.fetch(pda);
+        };
+
+        console.log("Success.");
+        console.log("Data:")
+        console.log(`    Color: ${data.color}   Balance: ${data.balance}`);
+        console.log(`Modifying balance of ${data.color} from ${data.balance} to ${newBalance}`);
+
+        await this.context.program.methods.modifyLedger(newBalance)
+            .accounts({
+                ledgerAccount: pda,
+                wallet: wallet.publicKey,
+            })
+            .signers([wallet])
+            .rpc();
+
+        data = await this.context.program.account.ledger.fetch(pda);
+        console.log("New Data:")
+        console.log(`    Color: ${data.color}   Balance: ${data.balance}`);
+        console.log("Success.");
+    }
+
+    private async derivePda(color: string, pubkey: anchor.web3.PublicKey) {
+        let [pda, _] = await anchor.web3.PublicKey.findProgramAddress(
+            [
+                pubkey.toBuffer(),
+                Buffer.from("_"),
+                Buffer.from(color),
+            ],
+            this.context.program.programId
+        );
+        return pda;
+    }
+
+    private async createLedgerAccount(
+        color: string,
+        pda: anchor.web3.PublicKey,
+        wallet: anchor.web3.Keypair
+    ) {
+        await this.context.program.methods.createLedger(color)
+            .accounts({
+                ledgerAccount: pda,
+                wallet: wallet.publicKey,
+            })
+            .signers([wallet])
+            .rpc();
+    }
 }
--- app/main.ts
+++ app/main.ts
@@ -1,86 +1,19 @@
 import * as anchor from "@project-serum/anchor";
-import { SolanaPdAs } from "../target/types/solana_pd_as";
 import { Context } from "./utils/context";
+import { Ledger } from "./ledger/ledger";
 
 async function main() {
     const context: Context = new Context();
+    const ledger: Ledger = new Ledger(context);
 
-    async function derivePda(color: string, pubkey: anchor.web3.PublicKey) {
-        let [pda, _] = await anchor.web3.PublicKey.findProgramAddress(
-            [
-                pubkey.toBuffer(),
-                Buffer.from("_"),
-                Buffer.from(color),
-            ],
-            context.program.programId
-        );
-        return pda;
-    }
+    const john: anchor.web3.Keypair = await context.generateKeypair();
+    await ledger.modifyLedger("red", 2, john);
+    await ledger.modifyLedger("red", 4, john);
+    await ledger.modifyLedger("blue", 2, john);
 
-    async function createLedgerAccount(
-        color: string,
-        pda: anchor.web3.PublicKey,
-        wallet: anchor.web3.Keypair
-    ) {
-        await context.program.methods.createLedger(color)
-            .accounts({
-                ledgerAccount: pda,
-                wallet: wallet.publicKey,
-            })
-            .signers([wallet])
-            .rpc();
-    }
-
-    async function modifyLedger(
-        color: string,
-        newBalance: number,
-        wallet: anchor.web3.Keypair,
-    ) {
-
-        console.log("--------------------------------------------------");
-        let data;
-        let pda = await derivePda(color, wallet.publicKey);
-
-        console.log(`Checking if account ${pda} exists for color: ${color}...`);
-        try {
-
-            data = await context.program.account.ledger.fetch(pda);
-            console.log("It does.");
-
-        } catch (e) {
-
-            console.log("It does NOT. Creating...");
-            await createLedgerAccount(color, pda, wallet);
-            data = await context.program.account.ledger.fetch(pda);
-        };
-
-        console.log("Success.");
-        console.log("Data:")
-        console.log(`    Color: ${data.color}   Balance: ${data.balance}`);
-        console.log(`Modifying balance of ${data.color} from ${data.balance} to ${newBalance}`);
-
-        await context.program.methods.modifyLedger(newBalance)
-            .accounts({
-                ledgerAccount: pda,
-                wallet: wallet.publicKey,
-            })
-            .signers([wallet])
-            .rpc();
-
-        data = await context.program.account.ledger.fetch(pda);
-        console.log("New Data:")
-        console.log(`    Color: ${data.color}   Balance: ${data.balance}`);
-        console.log("Success.");
-    }
-
-    const testKeypair1 = await context.generateKeypair();
-    await modifyLedger("red", 2, testKeypair1);
-    await modifyLedger("red", 4, testKeypair1);
-    await modifyLedger("blue", 2, testKeypair1);
-
-    const testKeypair2 = await context.generateKeypair();
-    await modifyLedger("red", 3, testKeypair2);
-    await modifyLedger("green", 3, testKeypair2);
+    const paul: anchor.web3.Keypair = await context.generateKeypair();
+    await ledger.modifyLedger("red", 3, paul);
+    await ledger.modifyLedger("green", 3, paul);
 }
 
 main().then(
